3576eda296162d68f50165a9ff110807507df880
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    nio: Migrate to our power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- device.mk
+++ device.mk
@@ -413,7 +413,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.motokona-libperfmgr
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # QCOM
 PRODUCT_COPY_FILES += \
