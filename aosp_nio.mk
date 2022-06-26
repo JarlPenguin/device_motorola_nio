4beed8513a9ae7b2af69f65576c089864b7ed619
Jarl-Penguin <jarlpenguin@outlook.com>
2022-06-26 11:54:45 +0300

    [DNM] nio: Customize for personal builds
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I8fdaca4677c3a91e1531c2f86d8936889841d3c3

--- aosp_nio.mk
+++ aosp_nio.mk
@@ -22,6 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/nio/device.mk)
 
 # Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/extra/product.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
