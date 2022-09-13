4fdc60087ce743bc060517fed0dd635cc78d5800
Jarl-Penguin <jarlpenguin@outlook.com>
2022-09-13 19:28:24 +0300

    nio: Update build properties
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- aosp_nio.mk
+++ aosp_nio.mk
@@ -18,13 +18,13 @@ $(call inherit-product, device/motorola/nio/device.mk)
 PRODUCT_NAME := aosp_nio
 PRODUCT_DEVICE := nio
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := XT2125-4
+PRODUCT_MODEL := moto g(100)
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_SYSTEM_NAME := nio_retcn
+PRODUCT_SYSTEM_NAME := nio_retail
 
 # Build info
-BUILD_FINGERPRINT := "motorola/nio_retcn/nio:12/S1RN32.55-16-2/125d6-32b4dd:user/release-keys"
+BUILD_FINGERPRINT := "motorola/nio_retail/nio:12/S1RTS32.41-20-16-1-3/22842-f0a4cf:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="nio_retcn-user 12 S1RN32.55-16-2 125d6-32b4dd release-keys" \
+    PRIVATE_BUILD_DESC="nio_retail-user 12 S1RTS32.41-20-16-1-3 22842-f0a4cf release-keys" \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
